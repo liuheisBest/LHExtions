cab7974507904150261ef6ba8467db9438eba122
LucyCZBK <your@email>
2016-12-15 17:09:10 +0800

    Initial Commit

---
+++ BaseViewControllers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaseViewControllers
+//
+//  Created by 刘贺 on 16/12/15.
+//  Copyright © 2016年 com.poba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BaseViewControllers/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BaseViewControllers
+//
+//  Created by 刘贺 on 16/12/15.
+//  Copyright © 2016年 com.poba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
